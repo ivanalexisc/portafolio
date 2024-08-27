896644b238827b91fb8cd02061d3c87c6ac4efa1
ivanalexisc <iac_1999@hotmail.com>
2024-08-27 20:27:37 -0300

    seccion hero y about

--- portafolio-react/src/components/Navbar.jsx
+++ portafolio-react/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ()=>{
     return (
         <nav className=" mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-            <img  className='mx-2 w-10' src={logo}  />
+            <img  className='mx-2 -w10' src={logo}  />
             </div>
             <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
              <FaLinkedin/>   
